08f35d613a887d79d11a127656e8831fd7076491
Mac-ziis <macg286@gmail.com>
2023-10-19 17:36:02 -0700

    Add Migrations and updates to the EngineersController

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -8,31 +8,31 @@ namespace Factory.Controllers
 {
   public class EngineersController : Controller
   {
-    // private readonly ToDoListContext _db;
+    private readonly FactoryContext _db;
 
-    // public CategoriesController(ToDoListContext db)
-    // {
-    //   _db = db;
-    // }
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
 
-    // public ActionResult Index()
-    // {
-    //   List<Category> model = _db.Categories.ToList();
-    //   return View(model);
-    // }
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers.ToList();
+      return View(model);
+    }
 
-    // public ActionResult Create()
-    // {
-    //   return View();
-    // }
+    public ActionResult Create()
+    {
+      return View();
+    }
 
-    // [HttpPost]
-    // public ActionResult Create(Category category)
-    // {
-    //   _db.Categories.Add(category);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public ActionResult Create(Engineer engineer)
+    {
+      _db.Engineers.Add(engineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
     // public ActionResult Details(int id)
     // {
@@ -44,25 +44,25 @@ namespace Factory.Controllers
     //   return View(thisCategory);
     // }
 
-    // public ActionResult Edit(int id)
-    // {
-    //   Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
-    //   return View(thisCategory);
-    // }
+    public ActionResult Edit(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
 
-    // [HttpPost]
-    // public ActionResult Edit(Category category)
-    // {
-    //   _db.Categories.Update(category);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public ActionResult Edit(Engineer engineer)
+    {
+      _db.Engineers.Update(engineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-    // public ActionResult Delete(int id)
-    // {
-    //   Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
-    //   return View(thisCategory);
-    // }
+    public ActionResult Delete(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
 
     // [HttpPost, ActionName("Delete")]
     // public ActionResult DeleteConfirmed(int id)
@@ -72,5 +72,35 @@ namespace Factory.Controllers
     //   _db.SaveChanges();
     //   return RedirectToAction("Index");
     // }
+
+    public ActionResult AddTag(int id)
+    // {
+    //   Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+    //   ViewBag.TagId = new SelectList(_db.Tags, "TagId", "Title");
+    //   return View(thisItem);
+    // }
+
+    // [HttpPost]
+    // public ActionResult AddTag(Item item, int tagId)
+    // {
+    //   #nullable enable
+    //   ItemTag? joinEntity = _db.ItemTags.FirstOrDefault(join => (join.TagId == tagId && join.ItemId == item.ItemId));
+    //   #nullable disable
+    //   if (joinEntity == null && tagId != 0)
+    //   {
+    //     _db.ItemTags.Add(new ItemTag() { TagId = tagId, ItemId = item.ItemId });
+    //     _db.SaveChanges();
+    //   }
+    //   return RedirectToAction("Details", new { id = item.ItemId });
+    // }   
+
+    // [HttpPost]
+    // public ActionResult DeleteJoin(int joinId)
+    // {
+    //   ItemTag joinEntry = _db.ItemTags.FirstOrDefault(entry => entry.ItemTagId == joinId);
+    //   _db.ItemTags.Remove(joinEntry);
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // } 
   }
 }
