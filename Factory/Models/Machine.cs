20f2a017b85399003026f4eebb53b13de6239476
Mac-ziis <macg286@gmail.com>
2023-10-21 10:12:22 -0700

    Add completed README.md

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -8,7 +8,6 @@ namespace Factory.Models
     public int MachineId { get; set; }
     [Required(ErrorMessage = "The machine's description can't be empty!")]
     public string Description { get; set; }
-    // [Range(1, int.MaxValue, ErrorMessage = "You must add your machine to an engineer. Have you created an engineer yet?")]
     public string Name { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
   }
