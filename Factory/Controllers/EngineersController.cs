841c665b880389a66bd5379f5b8321f31083fcd9
Mac-ziis <macg286@gmail.com>
2023-10-21 09:49:01 -0700

    Add the last touches to the Views and DeleteJoin to the Controllers

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -100,6 +100,15 @@ namespace Factory.Controllers
         _db.SaveChanges();
       }
       return RedirectToAction("Details", new { id = engineer.EngineerId });
-    }   
+    } 
+
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+      EngineerMachine joinEntry = _db.EngineerMachines.FirstOrDefault(entry => entry.EngineerMachineId == joinId);
+      _db.EngineerMachines.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }  
   }
 }
